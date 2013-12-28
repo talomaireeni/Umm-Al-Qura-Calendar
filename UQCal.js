a597eca685281dc9aefc89315aaf48a62092693c
talomaireeni <tariq@tadarrb.com>
2013-12-28 17:18:12 +0300

    Added repo url.

--- UQCal.js
+++ UQCal.js
@@ -2,6 +2,7 @@
 UQCal.js
 A script for converting Gregorian date to Hijri date (Umm Alqura calculation).
 Version: 1.0, 28/12/2013
+https://github.com/talomaireeni/Umm-Al-Qura-Calendar
 Licensed under the Apache License, Version 2.0 (the "License");
 You may not use this file except in compliance with the License.
 You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
